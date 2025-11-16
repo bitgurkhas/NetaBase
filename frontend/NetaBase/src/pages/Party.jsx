92e6af64da244c197de21cfbb0086abb954998f0
Sanjiv Thapa <thapasvt12@gmail.com>
2025-11-16 10:14:00 +0545

    Used axios instead of fetch

--- frontend/NetaBase/src/pages/Party.jsx
+++ frontend/NetaBase/src/pages/Party.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import  { useState, useEffect } from 'react';
 import axios from 'axios';
 import { ChevronRight, Loader2, AlertCircle } from 'lucide-react';
 
