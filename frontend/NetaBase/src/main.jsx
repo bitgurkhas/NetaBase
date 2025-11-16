22801c0fea8d1f0517fe0c48c47341dcfc592888
Sanjiv Thapa <thapasvt12@gmail.com>
2025-11-16 17:01:59 +0545

    Added vercel analytics in main.jsxx

--- frontend/NetaBase/src/main.jsx
+++ frontend/NetaBase/src/main.jsx
@@ -3,15 +3,14 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from '@vercel/speed-insights/react';
+import { SpeedInsights } from '@vercel/speed-insights/react'; 
 
 const root = createRoot(document.getElementById('root'));
 
 root.render(
   <StrictMode>
-    <App>
-      <SpeedInsights />
-      <Analytics />
-    </App>
+    <App /> 
+    <SpeedInsights /> 
+    <Analytics />
   </StrictMode>
 );
