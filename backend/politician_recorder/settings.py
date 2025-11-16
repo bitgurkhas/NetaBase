4df777c82306ca9a6f303151e29c7a221b6e7f8f
Sanjiv Thapa <thapasvt12@gmail.com>
2025-11-16 15:50:01 +0545

    Updated settings.py and created pytest.ini file and updated requirements

--- backend/politician_recorder/settings.py
+++ backend/politician_recorder/settings.py
@@ -3,13 +3,14 @@ from pathlib import Path
 from datetime import timedelta
 from dotenv import load_dotenv
 import dj_database_url
+from django.core.management.utils import get_random_secret_key
 
 load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Security
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY") or get_random_secret_key()
 DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "").split(",")
 
