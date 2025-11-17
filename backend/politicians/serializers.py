ca1008f34bbf8048b3ee870789f65ba129f92cc3
Sanjiv Thapa <thapasvt12@gmail.com>
2025-11-17 07:57:46 +0545

    New response on detail with promises and initiatives

--- backend/politicians/serializers.py
+++ backend/politicians/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from politicians.models import Party, Politician, Rating
+from politicians.models import Party, Politician, Rating, Initiatives, Promises
 from django.db.models import Avg
 
 
@@ -48,12 +48,29 @@ class RatingSerializer(serializers.ModelSerializer):
         fields = ['id', 'user_id', 'username', 'score', 'comment', 'created_at', 'updated_at']
         
 
+class InitiativesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Initiatives
+        fields = ["title", "description"]
+
+
+class PromisesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Promises
+        fields = ["title", "description", "status"]
+
+
 class PoliticianDetailSerializer(serializers.ModelSerializer):
     party_name = serializers.CharField(source='party.name', read_only=True)
+
+    initiatives = InitiativesSerializer(many=True, read_only=True)
+    promises = PromisesSerializer(many=True, read_only=True)
+
     class Meta:
         model = Politician
         fields = [
             "name", "slug", "photo", "views", "age", "education",
             "criminal_record", "party_name", "party_position", "criticism",
-            "location", "biography", "previous_party_history", "is_active", "average_rating"
+            "location", "biography", "previous_party_history", "is_active",
+            "average_rating", "initiatives", "promises"
         ]
